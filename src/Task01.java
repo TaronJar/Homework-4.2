c9465f2ddf6a99d2f210158ba97bc74251f911e0
TaronJar <taron64arm@gmail.com>
2023-11-22 20:38:45 +0400

    add commit Task01.java

---
+++ src/Task01.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println("Задание: 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество строк треугольника: ");
+        int rows = scanner.nextInt();
+
+        // Заполненный треугольник
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Пустой треугольник
+        for (int i = 0; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                if (i == rows || k == 1 || k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
